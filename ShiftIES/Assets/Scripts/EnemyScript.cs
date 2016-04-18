8336158755adc1a31312af5d49979e34579990d4
JET <jovitej@gmail.com>
2016-04-19 03:06:29 +0800

    Updated scripts for animations.

--- ShiftIES/Assets/Scripts/EnemyScript.cs
+++ ShiftIES/Assets/Scripts/EnemyScript.cs
@@ -5,10 +5,14 @@ public class EnemyScript : MonoBehaviour {
 
 	public Vector3 positionToGo;
 	public float speedOfMovement;
+	private Animator anim;
+	private SpriteRenderer sr;
 	//private GameObject formation;
 
 	// Use this for initialization
 	void Start () {
+		anim = GetComponent<Animator>();
+		sr = GetComponent<SpriteRenderer>();
 		//formation = GameObject.Find("Formation");
 	}
 	
@@ -18,6 +22,8 @@ public class EnemyScript : MonoBehaviour {
 		Vector2 moveVec2 = new Vector2(moveVec3.x, moveVec3.y);
 		if (moveVec2.magnitude > 1.0f) moveVec2.Normalize();
 		Vector3 finalMovement = new Vector3(moveVec2.x, moveVec2.y) * speedOfMovement;
+
+		anim.Play("enemyIdle"); // temp (add conditions later)
 
 		transform.position += finalMovement;
 	}
--- ShiftIES/Assets/Scripts/Unit1Script.cs
+++ ShiftIES/Assets/Scripts/Unit1Script.cs
@@ -10,12 +10,15 @@ public class Unit1Script : MonoBehaviour {
   private Animator anim;
 	private SpriteRenderer sr;
 	private GameObject formation;
-  
-	private int idleHash;
-	private int walkHash;
+
+	private float speedToQualifyAsRun;
+	//private int idleHash;
+	//private int walkHash;
+
 
 	// Use this for initialization
 	void Start () {
+		speedToQualifyAsRun = 0.5f;
 		anim = GetComponent<Animator>();
 		sr = GetComponent<SpriteRenderer>();
 		formation = GameObject.Find("Formation");
@@ -30,14 +33,19 @@ public class Unit1Script : MonoBehaviour {
 	void Update () {
 		Vector3 moveVec3 = positionToGo - transform.position;
 		Vector2 moveVec2 = new Vector2(moveVec3.x, moveVec3.y);
+		bool isRangeAnim = anim.GetCurrentAnimatorStateInfo(0).IsName("range");
 
-		// MAGIC NUMBER!
-		if(moveVec2.magnitude > 0.1f) anim.Play("walk");
-		else anim.Play("Idle");
+		if ((!isRangeAnim)  || (isRangeAnim && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f))
+		{
+			// MAGIC NUMBER!
+			print(moveVec2.magnitude);
+			if (moveVec2.magnitude > speedToQualifyAsRun) anim.Play("walk");
+			else anim.Play("Idle");
 
-		if (moveVec2.x > 0.0001f) sr.flipX = false;
-		else if (moveVec2.x < -0.0001f) sr.flipX = true;
-		
+			if (moveVec2.x > 0.0001f) sr.flipX = false;
+			else if (moveVec2.x < -0.0001f) sr.flipX = true;
+		}
+
 		if (moveVec2.magnitude > 1.0f) moveVec2.Normalize();
 		Vector3 finalMovement = new Vector3(moveVec2.x, moveVec2.y) * speedOfMovement * Time.deltaTime;
 		transform.position += finalMovement;
@@ -46,7 +54,12 @@ public class Unit1Script : MonoBehaviour {
   public void FireForEffect(float bulletSpeed)
   {
       FireBullet(transform, bulletSpeed);
-  }
+
+		if (Mathf.Cos(formation.transform.localRotation.eulerAngles.z * Mathf.Deg2Rad) >= 0) sr.flipX = false;
+		else sr.flipX = true;
+
+    anim.Play("range");
+	}
 
   void FireBullet(Transform trans, float bulletSpeed)
   {
