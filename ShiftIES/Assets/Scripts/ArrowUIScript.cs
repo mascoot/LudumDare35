a6fe0535ff6e13d5e59d7d85574151bfdfdf644c
JET <jovitej@gmail.com>
2016-04-18 14:22:05 +0800

    forgot.

---
+++ ShiftIES/Assets/Scripts/ArrowUIScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowUIScript : MonoBehaviour {
+
+	private GameObject formation;
+
+	// Use this for initialization
+	void Start () {
+		formation = GameObject.Find("Formation");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = formation.transform.position;
+		transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		transform.rotation = formation.transform.rotation;
+	}
+}
